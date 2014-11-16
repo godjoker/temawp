f7767e7424c98d4e79e359331cf828b404b07816
godjoker <arkangeltk26@gmail.com>
2014-11-16 03:37:48 -0500

    Archivos Base Tema Wordpress

---
+++ footer.php
@@ -0,0 +1,10 @@
+<footer>
+	<p>Desarrollado <?php bloginfo('name'); ?>, <?=date('Y');?>. Mi primer tema de WP.</p>
+
+</footer>
+
+</div> <!-- Fin de wrapper -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
